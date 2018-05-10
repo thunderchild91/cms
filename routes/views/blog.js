1a7176ab077abc8a36eb2d273a9c3a9065cd3942
unknown <amehmood@visualizeroi.com>
2018-05-10 20:10:37 +0500

    working on cms v1.0.1

--- routes/views/blog.js
+++ routes/views/blog.js
@@ -2,12 +2,12 @@ var keystone = require('keystone');
 var async = require('async');
 
 exports = module.exports = function (req, res) {
-
 	var view = new keystone.View(req, res);
 	var locals = res.locals;
-
+	
 	// Init locals
 	locals.section = 'blog';
+	
 	locals.filters = {
 		category: req.params.category,
 	};
@@ -56,7 +56,7 @@ exports = module.exports = function (req, res) {
 
 	// Load the posts
 	view.on('init', function (next) {
-
+		
 		var q = keystone.list('Post').paginate({
 			page: req.query.page || 1,
 			perPage: 10,
