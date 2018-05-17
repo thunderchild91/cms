86bd0d334cd85d6ea7d57afec7d1e390dbaf2926
unknown <amehmood@visualizeroi.com>
2018-05-18 02:34:34 +0500

    working on v1.0.2

--- models/User.js
+++ models/User.js
@@ -6,11 +6,64 @@ var Types = keystone.Field.Types;
  * ==========
  */
 var User = new keystone.List('User');
-
 User.add({
 	name: { type: Types.Name, required: true, index: true },
 	email: { type: Types.Email, initial: true, required: true, unique: true, index: true },
 	password: { type: Types.Password, initial: true, required: true },
+	ages: { type: Types.Select, options: [
+		{value: '13-17', label: '13-17'},
+		{value: '18+', label: '18+'}
+	] },
+	ageplay: { type: Types.Select, options: [
+		{value: 'Months', label: 'Months'},
+		{value: 'Years', label: 'Years'}
+	] },
+	gender: { type: Types.Select, options: [
+		{value: 'Female', label: 'Female'},
+		{value: 'MtF', label: 'MtF'},
+		{value: 'Male', label: 'Male'},
+		{value: 'FtM', label: 'FtM'},
+	] },
+	sexorientation: { type: Types.Select, options: [
+		{value: 'Straight', label: 'Straight'},
+		{value: 'Bisexual', label: 'Bisexual'},
+		{value: 'Gay', label: 'Gay'},
+		{value: 'Lesbian', label: 'Lesbian'},
+		{value: 'Pansexual', label: 'Pansexual'},
+		{value: 'Asexual', label: 'Asexual'},
+		{value: 'Polysexual', label: 'Polysexual'},
+	] },
+	yourType: { type: Types.Select, options: [
+		{value: 'Incontinence User', label: 'Incontinence User'},
+		{value: 'Adult Baby', label: 'Adult Baby'},
+		{value: 'BabyFur', label: 'BabyFur'},
+		{value: 'DiaperFur', label: 'DiaperFur'},
+		{value: 'Diaper Lover', label: 'Diaper Lover'},
+		{value: 'Mommy', label: 'Mommy'},
+		{value: 'Daddy', label: 'Daddy'},
+		{value: 'Domme', label: 'Domme'},
+		{value: 'Babysitter', label: 'Babysitter'},
+		{value: 'Little', label: 'Little'},
+		{value: 'Sissy', label: 'Sissy'},
+	] },
+	caregaver: { type: Types.Text, initial: true, required: true },
+	Rstatus: { type: Types.Select, options: [
+		{value: 'Single', label: 'Single'},
+		{value: 'Taken', label: 'Taken'},
+		{value: 'Engaged', label: 'Engaged'},
+		{value: 'Married', label: 'Married'},
+		{value: 'Other', label: 'Other'},
+	] },
+	address: { type: Types.Text, initial: true, required: true },
+	hobbies: { type: Types.Markdown, initial: true, required: true },
+	interests: { type: Types.Markdown, initial: true, required: true },
+	movies: { type: Types.Markdown, initial: true, required: true },
+	books: { type: Types.Markdown, initial: true, required: true },
+	music: { type: Types.Markdown, initial: true, required: true },
+	diapers: { type: Types.Markdown, initial: true, required: true },
+	stuffies: { type: Types.Markdown, initial: true, required: true },
+	pacifiers: { type: Types.Markdown, initial: true, required: true },
+	message: { type: Types.Markdown, initial: true, required: true },
 }, 'Permissions', {
 	isAdmin: { type: Boolean, label: 'Can access Keystone', index: true },
 });
--- routes/views/register.js
+++ routes/views/register.js
@@ -1,5 +1,6 @@
 var keystone = require('keystone');
 var Enquiry = keystone.list('Enquiry');
+var Useer = keystone.list('User');
 
 exports = module.exports = function (req, res) {
 
@@ -7,30 +8,139 @@ exports = module.exports = function (req, res) {
 	var locals = res.locals;
 
 	// Set locals
-	locals.section = 'contact';
+	locals.section = 'register';
 	locals.enquiryTypes = Enquiry.fields.enquiryType.ops;
 	locals.formData = req.body || {};
 	locals.validationErrors = {};
 	locals.enquirySubmitted = false;
 
+	locals.ages = Useer.fields.ages.ops;
+	locals.ageplay = Useer.fields.ageplay.ops;
+	locals.gender = Useer.fields.gender.ops;
+	locals.sexorientation = Useer.fields.sexorientation.ops;
+	locals.yourType = Useer.fields.yourType.ops;
+	locals.Rstatus = Useer.fields.Rstatus.ops;
+
+	// locals.ages = [
+	// 	{value: '13-17', label: '13-17'},
+	// 	{value: '18+', label: '18+'}
+	// ];
+	
+	// locals.ageplay = [
+	// 	{value: 'Months', label: 'Months'},
+	// 	{value: 'Years', label: 'Years'}
+	// ];
+	// locals.gender = [
+	// 	{value: 'Female', label: 'Female'},
+	// 	{value: 'MtF', label: 'MtF'},
+	// 	{value: 'Male', label: 'Male'},
+	// 	{value: 'FtM', label: 'FtM'},
+	// ];
+	// locals.sexorientation = [
+	// 	{value: 'Straight', label: 'Straight'},
+	// 	{value: 'Bisexual', label: 'Bisexual'},
+	// 	{value: 'Gay', label: 'Gay'},
+	// 	{value: 'Lesbian', label: 'Lesbian'},
+	// 	{value: 'Pansexual', label: 'Pansexual'},
+	// 	{value: 'Asexual', label: 'Asexual'},
+	// 	{value: 'Polysexual', label: 'Polysexual'},
+	// ];
+	// locals.yourType = [
+	// 	{value: 'Incontinence User', label: 'Incontinence User'},
+	// 	{value: 'Adult Baby', label: 'Adult Baby'},
+	// 	{value: 'BabyFur', label: 'BabyFur'},
+	// 	{value: 'DiaperFur', label: 'DiaperFur'},
+	// 	{value: 'Diaper Lover', label: 'Diaper Lover'},
+	// 	{value: 'Mommy', label: 'Mommy'},
+	// 	{value: 'Daddy', label: 'Daddy'},
+	// 	{value: 'Domme', label: 'Domme'},
+	// 	{value: 'Babysitter', label: 'Babysitter'},
+	// 	{value: 'Little', label: 'Little'},
+	// 	{value: 'Sissy', label: 'Sissy'},
+	// ];
+	// locals.Rstatus = [
+	// 	{value: 'Single', label: 'Single'},
+	// 	{value: 'Taken', label: 'Taken'},
+	// 	{value: 'Engaged', label: 'Engaged'},
+	// 	{value: 'Married', label: 'Married'},
+	// 	{value: 'Other', label: 'Other'},
+	// ];
+	
 	// On POST requests, add the Enquiry item to the database
-	view.on('post', { action: 'contact' }, function (next) {
+	view.on('post', { action: 'register' }, function (next) {
+		var User = keystone.list('User').model;
 
-		var newEnquiry = new Enquiry.model();
-		var updater = newEnquiry.getUpdateHandler(req);
-
-		updater.process(req.body, {
-			flashErrors: true,
-			fields: 'name, email, phone, enquiryType, message',
-			errorMessage: 'There was a problem submitting your enquiry:',
-		}, function (err) {
+		var user = new User({
+            name: {
+                first: locals.formData.first
+			},
+			email: locals.formData.email,
+			password: locals.formData.password,
+			ages: locals.formData.ages,
+			ageplay: locals.formData.ageplay,
+			gender: locals.formData.gender,
+			sexorientation: locals.formData.sexorientation,
+			yourType: locals.formData.yourType,
+			caregaver: locals.formData.caregaver,
+			Rstatus: locals.formData.Rstatus,
+			address: locals.formData.address,
+			hobbies: locals.formData.hobbies,
+			interests: locals.formData.interests,
+			movies: locals.formData.movies,
+			books: locals.formData.books,
+			music: locals.formData.music,
+			diapers: locals.formData.diapers,
+			stuffies: locals.formData.stuffies,
+			pacifiers: locals.formData.pacifiers,
+			message: locals.formData.message,
+			isAdmin: false
+        });
+		user.save(function (err) {
 			if (err) {
+				// handle error
 				locals.validationErrors = err.errors;
-			} else {
-				locals.enquirySubmitted = true;
 			}
-			next();
+		
+			// user has been saved
+			console.log(user);
+			res.redirect('/keystone/signin');
+			// keystone.redirect('/keystone/signin');
+			// redirect('/');
+			
 		});
+        // var updater = newUser.getUpdateHandler(req);
+
+        // updater.process(req.body, {
+        //     fields: 'email, password',
+        //     flashErrors: true,
+        //     logErrors: true
+        // }, function(err,result) {
+        //     if (err) {      
+        //         data.validationErrors = err.errors; 
+        //     } else {
+        //         req.flash('success', 'Account created. Please sign in.');               
+        //         return res.redirect('/');
+        //     }
+        //     next();
+        // });
+
+
+		// var newEnquiry = new Enquiry.model();
+		// var updater = newEnquiry.getUpdateHandler(req);
+		// updater.process(req.body, {
+		// 	flashErrors: true,
+		// 	fields: 'name, email, ages, ageplay, gender, sexorientation, yourType, Rstatus, caregaver, address, hobbies, interests, movies, books, music, diapers, stuffies, pacifiers, message',
+		// 	errorMessage: 'There was a problem submitting your enquiry:',
+		// }, function (err) {
+		// 	if (err) {
+		// 		console.log("TESTING");
+		// 		locals.validationErrors = err.errors;
+		// 	} else {
+		// 		console.log("TEST");
+		// 		// locals.enquirySubmitted = true;
+		// 	}
+		// 	next();
+		// });
 	});
 
 	view.render('register');
