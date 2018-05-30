e535a0c6ca751d7a9a84d36fb67c361b0fb9e552
unknown <amehmood@visualizeroi.com>
2018-05-31 02:23:20 +0500

    working on v1.0.4

--- keystone.js
+++ keystone.js
@@ -42,6 +42,7 @@ keystone.set('locals', {
 // Load your project's Routes
 keystone.set('routes', require('./routes'));
 
+keystone.set('signin redirect', '/');
 
 // Configure the navigation bar in Keystone's Admin UI
 keystone.set('nav', {
