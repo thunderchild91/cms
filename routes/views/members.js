fb3d3e645ac6da19f4edc994d0066300a1438e82
unknown <amehmood@visualizeroi.com>
2018-06-08 01:06:29 +0500

    working on v1.0.6

--- routes/views/members.js
+++ routes/views/members.js
@@ -10,21 +10,44 @@ exports = module.exports = function (req, res) {
 
 	// Set locals
 	locals.section = 'members';
-	locals.data = {
-		posts: [],
-	};
-	// locals.posts = [];
-    User = keystone.list('User');
-	view.on('init', function (next) {
-		User.model.find()
-			.sort('_id')
-			.exec(function(err, posts) {
-				// do something with posts
-				locals.data.posts = posts;
-				// locals.data.posts.push(posts);
-				next(err);
-			});
-	});
+	// locals.data = {
+	// 	posts: [],
+	// };
+	// // locals.posts = [];
+    // User = keystone.list('User');
+	// view.on('init', function (next) {
+	// 	User.model.find()
+	// 		.sort('_id')
+	// 		.exec(function(err, posts) {
+	// 			// do something with posts
+	// 			locals.data.posts = posts;
+	// 			// locals.data.posts.push(posts);
+	// 			next(err);
+	// 		});
+	// });
+
+	// locals.data = {
+	// 	users: []
+	// };
+	// view.on('init', function (next) {
+	// 	var q = keystone.list('User').paginate({
+	// 		page: req.query.page || 1,
+	// 		perPage: 10,
+	// 		maxPages: 10,
+			
+	// 	})
+	// 		.sort('sortOrder')
+			
+
+	// 	q.exec(function (err, results) {
+	// 		// console.log(results);
+	// 		locals.data.users = results;
+	// 		next(err);
+	// 	});
+	// });
+
+
+	view.query('users', keystone.list('User').model.find().sort('sortOrder'));
 
 	view.render('members');
 };
