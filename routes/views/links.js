88c7b4ae7eb17be388dd6a0c7374cc9a141ab90f
unknown <amehmood@visualizeroi.com>
2018-06-29 04:59:32 +0500

    working on admin panel form fix(completed) v1.0.6

---
+++ routes/views/links.js
@@ -0,0 +1,16 @@
+var keystone = require('keystone');
+var async = require('async');
+
+exports = module.exports = function (req, res) {
+
+	var view = new keystone.View(req, res);
+	var locals = res.locals;
+
+	// locals.section is used to set the currently selected
+	// item in the header navigation.
+	locals.section = 'links';
+	
+
+	// Render the view
+	view.render('links');
+};
---
+++ routes/views/rules.js
@@ -0,0 +1,16 @@
+var keystone = require('keystone');
+var async = require('async');
+
+exports = module.exports = function (req, res) {
+
+	var view = new keystone.View(req, res);
+	var locals = res.locals;
+
+	// locals.section is used to set the currently selected
+	// item in the header navigation.
+	locals.section = 'rules';
+	
+
+	// Render the view
+	view.render('rules');
+};
